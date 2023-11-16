140d27d14afa72c94ebf3fbde3c82a88c66b1408
Dinnu <nitin2239@protonmail.com>
2023-11-16 16:26:47 +0530

    download this 2 files and run at start of your Drive

---
+++ [WINDOWS]delete_unwantedfiles/del_unwanted.ps1
@@ -0,0 +1,17 @@
+# PowerShell commands
+$items = Get-ChildItem -Path . -Include "work", "*.mpf","*qverilog.log", "*.vstf","*.mti", "*.wlf" ,"*.vhd" ,"*.dbg" ,"transcript" -Recurse
+
+$items | ForEach-Object {
+    $item = $_
+    if ($item.PSIsContainer) {
+        Write-Output "Folder: $($item.FullName)"
+    } else {
+        Write-Output "File: $($item.FullName)"
+    }
+}
+
+$items | Remove-Item -Recurse -Force
+
+# Completion message
+Write-Output "Script completed successfully."
+
---
+++ [WINDOWS]delete_unwantedfiles/runscript.cmd
@@ -0,0 +1,5 @@
+@echo off
+PowerShell.exe -ExecutionPolicy Bypass -File "%~dp0del_unwanted.ps1"
+echo Script completed successfully.
+pause
+
